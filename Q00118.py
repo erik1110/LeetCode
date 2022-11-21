06520c7cb4065c8ab4e111183d26998952bc4bac
erik1110 <oceanuheart@gmail.com>
2022-11-22 01:06:11 +0800

    [feat] 118. Pascal's Triangle

---
+++ Q00118.py
@@ -0,0 +1,25 @@
+'''
+118. Pascal's Triangle
+
+Given an integer numRows, return the first numRows of Pascal's triangle.
+
+In Pascal's triangle, each number is the sum of the two numbers directly above it as shown:
+
+
+Example 1:
+
+Input: numRows = 5
+Output: [[1],[1,1],[1,2,1],[1,3,3,1],[1,4,6,4,1]]
+Example 2:
+
+Input: numRows = 1
+Output: [[1]]
+
+'''
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        pascal =[[1]*(i+1) for i in range(numRows)]
+        for i in range(numRows):
+            for j in range(1, i):
+                pascal[i][j] = pascal[i-1][j-1] + pascal[i-1][j]
+        return pascal
