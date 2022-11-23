3a8fdd7620ea3f809791d3d572c900e9ce543b2f
erik1110 <oceanuheart@gmail.com>
2022-11-24 01:46:03 +0800

    [feat] 242. Valid Anagram

---
+++ Q00242.py
@@ -0,0 +1,28 @@
+'''
+242. Valid Anagram
+
+Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+
+An Anagram is a word or phrase formed by rearranging the letters of a different word or phrase, typically using all the original letters exactly once.
+
+
+Example 1:
+
+Input: s = "anagram", t = "nagaram"
+Output: true
+
+Example 2:
+
+Input: s = "rat", t = "car"
+Output: false
+
+'''
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if set(s) == set(t):
+            for i in s:
+                if s.count(i) != t.count(i):
+                    return False
+            return True
+        else:
+            return False
