aa8ff732130f05db1c3452981d93dceaad38962c
erik1110 <oceanuheart@gmail.com>
2022-11-27 22:59:24 +0800

    [feat] 20. Valid Parentheses

---
+++ Q00020.py
@@ -0,0 +1,41 @@
+'''
+20. Valid Parentheses
+
+Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+
+An input string is valid if:
+
+Open brackets must be closed by the same type of brackets.
+Open brackets must be closed in the correct order.
+Every close bracket has a corresponding open bracket of the same type.
+ 
+
+Example 1:
+Input: s = "()"
+Output: true
+
+Example 2:
+Input: s = "()[]{}"
+Output: true
+
+Example 3:
+Input: s = "(]"
+Output: false
+'''
+class Solution:
+    def isValid(self, s: str) -> bool:
+        open_tup = tuple('({[')
+        close_tup = tuple(')}]')
+        map = dict(zip(open_tup, close_tup))
+        queue = []
+
+        for i in s:
+            if i in open_tup:
+                queue.append(map[i])
+            elif i in close_tup:
+                if not queue or i != queue.pop():
+                    return False
+        if not queue:
+            return True
+        else:
+            return False
