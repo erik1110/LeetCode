38423c74343962c7d5c561d0c92e6ff9a9008e1e
erik1110 <oceanuheart@gmail.com>
2023-01-19 00:45:25 +0800

    [docs] add docs

--- algo/sortedSquaredArray.py
+++ algo/sortedSquaredArray.py
@@ -1,3 +1,10 @@
+'''
+
+Write a function that takes in a non-empty array of integers that are sorted
+in ascending order and returns a new array of the same length with the squares
+of the original integers also sorted in ascending order.
+
+'''
 def sortedSquaredArray(array):
     res = []
     left = 0
