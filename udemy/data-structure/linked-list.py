dc30ffbdc6f902cc011165f27b345278bd0cddfc
erik1110 <oceanuheart@gmail.com>
2024-02-26 15:15:50 +0800

    [feat] add linked_list

---
+++ udemy/data-structure/linked-list.py
@@ -0,0 +1,37 @@
+class Node:
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+        
+class LinkedList:
+    def __init__(self):
+        self.head = None
+        self.length = 0
+        
+    def push(self, value):
+        new_node = Node(value)
+        if not self.head:
+            self.head = new_node
+        else:
+            current_node = self.head
+            # 看 current_node 是不是最後一個，不是就繼續 next
+            # 是最後一個，next 就連接上新的 new_node
+            while current_node.next:
+                current_node = current_node.next
+            current_node.next = new_node
+        self.length += 1
+        
+    def print_list(self):
+        current_node = self.head
+        while current_node:
+            print(current_node.value, end=" ")
+            current_node = current_node.next
+        print()
+        
+        
+if __name__ == "__main__":
+    myLinkedList = LinkedList()
+    myLinkedList.push("Mike")
+    myLinkedList.push("Ann")
+    myLinkedList.push("Emily")
+    myLinkedList.print_list()
