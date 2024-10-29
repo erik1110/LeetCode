7399842ade388852054aeb4b5c5cf7878aa4a050
erik1110 <oceanuheart@gmail.com>
2024-10-29 07:27:40 +0000

    feat: add Q00187

---
+++ leetcode_uk/Q00187.py
@@ -0,0 +1,37 @@
+'''
+187. Repeated DNA Sequences
+
+The DNA sequence is composed of a series of nucleotides abbreviated as 'A', 'C', 'G', and 'T'.
+
+For example, "ACGAATTCCG" is a DNA sequence.
+When studying DNA, it is useful to identify repeated sequences within the DNA.
+
+Given a string s that represents a DNA sequence, return all the 10-letter-long sequences (substrings) that occur more than once in a DNA molecule. You may return the answer in any order.
+
+Example 1:
+
+Input: s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT"
+Output: ["AAAAACCCCC","CCCCCAAAAA"]
+Example 2:
+
+Input: s = "AAAAAAAAAAAAA"
+Output: ["AAAAAAAAAA"]
+'''
+
+class Solution(object):
+    def findRepeatedDnaSequences(self, s):
+        """
+        :type s: str
+        :rtype: List[str]
+        """
+        if len(s) <= 10:
+            return []
+        substring_list = []
+        seen, repeated = set(), set()
+        for i in range(len(s)-10+1):
+            substring = s[i:i+10]
+            if substring in seen:
+                repeated.add(substring)
+            else:
+                seen.add(substring)
+        return list(repeated)
