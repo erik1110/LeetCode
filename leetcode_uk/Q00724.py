61c6b483ab41516455a3beae8580c3512ad27325
erik1110 <oceanuheart@gmail.com>
2024-11-06 08:51:01 +0000

    feat: add pivotIndex

---
+++ leetcode_uk/Q00724.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def pivotIndex(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        left_sum = 0
+        total_sum = sum(nums)
+        for i in range(len(nums)):
+            right_sum = total_sum - left_sum - nums[i]
+            if left_sum == right_sum:
+                return i
+            left_sum += nums[i]
+
+        return -1
